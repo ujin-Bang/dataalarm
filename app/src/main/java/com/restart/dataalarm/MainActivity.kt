77c1368007e3ad709020023ae78a518822942b99
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-08-04 15:40:51 +0900

    파이어베이스 클라우드메시지 설정 + 토큰값 받아오기 + firebase홈페이지에서 받아온 토큰값을 이용해 알림네시지 보내기

--- app/src/main/java/com/restart/dataalarm/MainActivity.kt
+++ app/src/main/java/com/restart/dataalarm/MainActivity.kt
@@ -2,7 +2,9 @@ package com.restart.dataalarm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,12 +12,26 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.resultTextView)
     }
 
-    private val firebase: TextView by lazy {
+    private val firebaseToken: TextView by lazy {
         findViewById(R.id.firebaseTokenTextView)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initFirebase()
+
+    }
+
+    //파이어 베이스 토큰 불러오기
+    private fun initFirebase(){
+        FirebaseMessaging.getInstance().token
+            .addOnCompleteListener {
+                if (it.isSuccessful){
+                    firebaseToken.text = it.result
+                    Log.d("파이어베이스 토큰값",it.result.toString())
+                }
+            }
     }
 }
