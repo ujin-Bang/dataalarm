a7ff80085e49a31470b1d75e4dab21364f9f5790
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-08-04 14:48:58 +0900

    UI, kt 연결

--- app/src/main/java/com/restart/dataalarm/MainActivity.kt
+++ app/src/main/java/com/restart/dataalarm/MainActivity.kt
@@ -2,8 +2,18 @@ package com.restart.dataalarm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private val resultTextView: TextView by lazy {
+        findViewById(R.id.resultTextView)
+    }
+
+    private val firebase: TextView by lazy {
+        findViewById(R.id.firebaseTokenTextView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
